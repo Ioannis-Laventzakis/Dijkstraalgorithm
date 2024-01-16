2a134b57b38c4d93eb5b0f97034d7c723521d4af
Ioannis84 <glaventzakis@gmail.com>
2024-01-16 11:39:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,89 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating nodes
+        Node nodeA = new Node("A");
+        Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
+        // Add destinations and distances
+        nodeA.addDestination(nodeB, 10);
+        nodeA.addDestination(nodeC, 15);
+        nodeB.addDestination(nodeC, 5);
+
+        // Creating a graph and adding nodes
+        Graph graph = new Graph();
+        graph.addNode(nodeA);
+        graph.addNode(nodeB);
+        graph.addNode(nodeC);
+
+        // Calculate the shortest path from source (e.g., nodeA)
+        graph = calculateShortestPathFromSource(graph, nodeA);
+
+        // Print the shortest paths and distances for each node...
+        for (Node node : graph.getNodes()) {
+            System.out.println("Shortest Path to " + node.getName() + ": " + node.getShortestPath());
+            System.out.println("Shortest Distance to " + node.getName() + ": " + node.getDistance());
+        }
+    }
+
+    private static Graph calculateShortestPathFromSource(Graph graph, Node source) {
+        source.setDistance(0);
+
+        Set<Node> settledNodes = new HashSet<>();
+        Set<Node> unsettledNodes = new HashSet<>();
+
+        unsettledNodes.add(source);
+
+        while (!unsettledNodes.isEmpty()) {
+            Node currentNode = getLowestDistanceNode(unsettledNodes);
+            unsettledNodes.remove(currentNode);
+
+            for (Entry<Node, Integer> adjacencyPair : currentNode.getAdjacentNodes().entrySet()) {
+                Node adjacentNode = adjacencyPair.getKey();
+                Integer edgeWeight = adjacencyPair.getValue();
+
+                if (!settledNodes.contains(adjacentNode)) {
+                    calculateMinimumDistance(adjacentNode, edgeWeight, currentNode);
+                    unsettledNodes.add(adjacentNode);
+                }
+            }
+            settledNodes.add(currentNode);
+        }
+
+        return graph;
+    }
+
+    private static Node getLowestDistanceNode(Set<Node> unsettledNodes) {
+        Node lowestDistanceNode = null;
+        int lowestDistance = Integer.MAX_VALUE;
+
+        for (Node node : unsettledNodes) {
+            int nodeDistance = node.getDistance();
+            if (nodeDistance < lowestDistance) {
+                lowestDistance = nodeDistance;
+                lowestDistanceNode = node;
+            }
+        }
+
+        return lowestDistanceNode;
+    }
+
+    private static void calculateMinimumDistance(Node evaluationNode, int edgeWeight, Node sourceNode) {
+        int sourceDistance = sourceNode.getDistance();
+
+        if (sourceDistance + edgeWeight < evaluationNode.getDistance()) {
+            evaluationNode.setDistance(sourceDistance + edgeWeight);
+
+            // Update the shortest path
+            List<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+            shortestPath.add(sourceNode);
+            evaluationNode.setShortestPath(shortestPath);
+        }
+    }
+}
